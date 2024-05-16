fba333a541339ddea26566a959a22fb74ac6b53d
Mariaescapereality <145456229+Mariaescapereality@users.noreply.github.com>
2024-05-16 15:36:55 +0200

    Add files via upload

---
+++ class/PDO/blogPDO.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once dirname(__DIR__,2) . '/includes/init.php';
+
+class BlogPDO extends PDO
+{
+
+    public function __construct()
+    {
+        $db_port = !empty(DB_PORT) ? ';port=' . DB_PORT : '';
+        $db_dns = DB_TYPE . ':host=' . DB_HOST . $db_port . ';dbname=' . DB_NAME;
+        parent::__construct($db_dns, DB_USERNAME, DB_PASSWORD, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+        ]);
+    }
+    public function pluralize(string $word)
+    {
+        switch ($word) {
+            case 'post':
+                return 'posts';
+                break;
+            case 'category':
+                return 'categories';
+                break;
+            case 'author':
+                return 'authors';
+                break;
+            default:
+                return '';
+        }
+    }
+}
+
+
+
